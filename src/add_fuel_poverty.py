143f2e48a8195274b71dd07440945036196fb88d
Lena Kilian <43342015+lena-kilian@users.noreply.github.com>
2023-08-22 19:35:50 -0400

    Update add_fuel_poverty.py

--- src/add_fuel_poverty.py
+++ src/add_fuel_poverty.py
@@ -8,6 +8,7 @@ Created on Tue Aug  1 17:59:26 2023
 import pandas as pd
 import copy as cp
 import matplotlib.pyplot as plt
+import numpy as np
 
 output_path = 'C:/Users/geolki/OneDrive - University of Leeds/Postdoc/Ageing_project/analysis/'
 
@@ -66,7 +67,8 @@ for fp in fp_defs:
 demographics = {} 
 for fp in fp_defs:
     demographics[fp] = pd.DataFrame()
-    for item in list(ghg_data.keys()):
+    for yr in range(2007, 2020):
+        item = str(yr)
         ghg_data[item]['energy_spend'] = ghg_data[item].loc[:, 'spend_4.5.1_Combined electricity meter payment less rebate':
                                                             'spend_4.5.5_Hot water, steam and ice'].sum(1)
         ghg_data[item]['fuel_poor'] = False
@@ -75,11 +77,12 @@ for fp in fp_defs:
         temp = cp.copy(ghg_data[item])
         temp.loc[temp['hhd_type_1_gender'].isna() == True, 'hhd_type_1_gender'] = 'Other'
         temp = temp.fillna(0).sort_values(['hhd_type', 'fuel_poor', 'hhd_type_1_gender']).set_index(['hhd_type', 'fuel_poor', 'hhd_type_1_gender'])
+        temp['age_all'] = temp['age_all'].str.replace('nan', 'np.nan')
         temp['age_mean'] = [sum(eval(x))/len(eval(x)) for x in temp['age_all']]
         
         mean_w = cp.copy(temp)
         vars_mean_w = ['Income anonymised', 'income tax', 'energy_spend']
-        mean_w[vars_mean_w] = mean_w[vars_mean_w].apply(lambda x: x*mean_w['weight'])
+        mean_w[vars_mean_w] = mean_w[vars_mean_w].apply(lambda x: x*mean_w['weight']*mean_w['no_people'])
         mean_w = mean_w.sum(axis=0, level=[0, 1, 2])
         mean_w = mean_w[vars_mean_w].apply(lambda x: x/(mean_w['weight'] * mean_w['OECD scale']))
         
@@ -87,18 +90,22 @@ for fp in fp_defs:
         vars_mean = ['age_mean', 'rooms in accommodation']
         mean[vars_mean] = mean[vars_mean].apply(lambda x: x*mean['weight'])
         mean = mean.sum(axis=0, level=[0, 1, 2])
-        mean = mean[vars_mean].apply(lambda x: x/(mean['weight'] * mean['no_people']))
+        mean = mean[vars_mean].apply(lambda x: x/(mean['weight']))
         
         count = cp.copy(temp)
         vars_count = ['GOR', 'OA class 1', 'home_ownership', 'ethnicity hrp', 'ethnicity partner hrp']
         count = count[vars_count + ['weight']].reset_index()
-        count_results = count.groupby(['hhd_type', 'fuel_poor', 'hhd_type_1_gender'])[['weight']].sum()
-        for item in vars_count:
+        count['count'] = 1
+        count_results = count.groupby(['hhd_type', 'fuel_poor', 'hhd_type_1_gender'])[['weight', 'count']].sum()
+        count_results['pct_of_hhlds'] = count_results['weight'] / count_results['weight'].sum() *100
+        count_results['pct_of_surveys'] = count_results['count'] / count_results['count'].sum() *100
+        for var in vars_count:
             temp2 = cp.copy(count)
-            temp2['count_' + item] = 1
-            temp2 = temp2.groupby(['hhd_type', 'fuel_poor', 'hhd_type_1_gender', item])[['count_' + item, 'weight']].sum()
-            temp2['count_' + item] = temp2['count_' + item] * temp2['weight']
-            temp2 = temp2[['count_' + item]].unstack(item).fillna(0)
+            temp[var] = temp[var].astype(str)
+            temp2['count_' + var] = 1
+            temp2 = temp2.groupby(['hhd_type', 'fuel_poor', 'hhd_type_1_gender', var])[['count_' + var, 'weight']].sum()
+            temp2['count_' + var] = temp2['count_' + var] * temp2['weight']
+            temp2 = temp2[['count_' + var]].unstack(var).fillna(0)
             totals = temp2.sum(axis=1)
             for col in temp2.columns:
                 temp2[col] = temp2[col] / totals * 100
@@ -108,5 +115,17 @@ for fp in fp_defs:
         temp = mean_w.join(mean).join(count_results)
         temp['year'] = int(item)
         
-        demographics[fp] = demographics[fp].append(temp)
+        demographics[fp] = demographics[fp].append(temp).fillna(0)
+    
+demographics_all = pd.DataFrame()
+for fp in fp_defs:
+    temp = cp.copy(demographics[fp])
+    temp['FP_def'] = fp
+    demographics_all = demographics_all.append(temp.reset_index())
+    
+demographics_all_filter = demographics_all.loc[demographics_all['year'] == 2019]\
+    .set_index(['hhd_type', 'hhd_type_1_gender', 'fuel_poor', 'FP_def'])\
+    .unstack(level='FP_def')[['Income anonymised', 'energy_spend', 'age_mean', 'rooms in accommodation', 
+                              'pct_of_hhlds', 'pct_of_surveys']]
+    
     
